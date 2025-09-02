08976a81e83a235dd621eaab8244dbf55f94f3c4
Kawshik Biswas <kawshikbss@gmail.com>
2025-09-03 01:45:23 +0600

    modified header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -80,65 +80,63 @@ export const data = {
 export default function Dashboard() {
   return (
     <main className="p-8">
-      <div className="my-6">
-        <div className="w-full flex flex-row justify-between items-center">
-          <h3 className="text-2xl font-medium">Dashboard</h3>
+      <div className="w-full flex flex-row justify-between items-center">
+        <h3 className="text-2xl font-medium">Dashboard</h3>
+        <Link
+          href="/"
+          className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md"
+        >
+          + Add New Product
+        </Link>
+      </div>
+      <div className="my-6 w-full flex flex-row justify-between gap-4">
+        <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+          <div className="flex flex-row justify-between items-center">
+            <h4 className="text-lg font-normal">Todays Sales</h4>
+          </div>
+          <h3 className="text-2xl font-semibold">$10,000</h3>
+          <span className="text-sm text-green-500 flex items-center">
+            <FaCaretUp /> 70%
+          </span>
+        </div>
+        <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+          <div className="flex flex-row justify-between items-center">
+            <h4 className="text-lg font-normal">Todays Orders</h4>
+          </div>
+          <h3 className="text-2xl font-semibold">+ 10,000</h3>
+          <span className="text-sm text-green-500 flex items-center">
+            <FaCaretUp /> 70%
+          </span>
+        </div>
+        <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+          <div className="flex flex-row justify-between items-center">
+            <h4 className="text-lg font-normal">Top Product</h4>
+          </div>
           <Link
             href="/"
-            className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md"
+            className="flex flex-row justify-between items-center my-2"
           >
-            + Add New Product
+            <Image
+              src="https://globalcare.com.bd/public/uploads/all/u0KM6G8OypRGBAJ1YxNk0mbpi9zhEbRWfa1ogSm0.jpg"
+              alt="Sergel 20mg"
+              width={60}
+              height={40}
+              className="aspect-3/2 object-cover rounded-lg"
+            />
+            <h3 className="text-lg font-semibold">Sergel 20mg</h3>
+          </Link>
+          <Link href="/" className="text-sm text-blue-500 flex items-center">
+            See More
           </Link>
         </div>
-        <div className="mt-6 w-full flex flex-row justify-between gap-4">
-          <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
-            <div className="flex flex-row justify-between items-center">
-              <h4 className="text-lg font-normal">Todays Sales</h4>
-            </div>
-            <h3 className="text-2xl font-semibold">$10,000</h3>
-            <span className="text-sm text-green-500 flex items-center">
-              <FaCaretUp /> 70%
-            </span>
+        <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+          <div className="flex flex-row justify-between items-center">
+            <h4 className="text-lg font-normal">Low Stocks</h4>
           </div>
-          <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
-            <div className="flex flex-row justify-between items-center">
-              <h4 className="text-lg font-normal">Todays Orders</h4>
-            </div>
-            <h3 className="text-2xl font-semibold">+ 10,000</h3>
-            <span className="text-sm text-green-500 flex items-center">
-              <FaCaretUp /> 70%
-            </span>
-          </div>
-          <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
-            <div className="flex flex-row justify-between items-center">
-              <h4 className="text-lg font-normal">Top Product</h4>
-            </div>
-            <Link
-              href="/"
-              className="flex flex-row justify-between items-center my-2"
-            >
-              <Image
-                src="https://globalcare.com.bd/public/uploads/all/u0KM6G8OypRGBAJ1YxNk0mbpi9zhEbRWfa1ogSm0.jpg"
-                alt="Sergel 20mg"
-                width={60}
-                height={40}
-                className="aspect-3/2 object-cover rounded-lg"
-              />
-              <h3 className="text-lg font-semibold">Sergel 20mg</h3>
-            </Link>
-            <Link href="/" className="text-sm text-blue-500 flex items-center">
-              See More
-            </Link>
-          </div>
-          <div className="w-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
-            <div className="flex flex-row justify-between items-center">
-              <h4 className="text-lg font-normal">Low Stocks</h4>
-            </div>
-            <h3 className="text-2xl font-semibold">- 10,000</h3>
-            <Link href="/" className="text-sm text-blue-500 flex items-center">
-              See More
-            </Link>
-          </div>
+          <h3 className="text-2xl font-semibold">- 10,000</h3>
+          <Link href="/" className="text-sm text-blue-500 flex items-center">
+            See More
+          </Link>
         </div>
       </div>
       <div className="my-6">
