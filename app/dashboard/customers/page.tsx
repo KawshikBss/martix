dba2c6050beed1ed1691d4b99f8cb79cdd0d33fc
Kawshik Biswas <kawshikbss@gmail.com>
2025-10-07 19:38:23 +0600

    created customers list page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,147 @@
+import KpiCard from "@/components/ui/KpiCard";
+import ordersData from "@/public/data/ordersData";
+import Link from "next/link";
+import * as React from "react";
+import { FaCheckSquare, FaMoneyBill, FaPlusSquare } from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
+
+export default function CustomersList() {
+  return (
+    <main className="p-8">
+      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-row justify-between items-center">
+        <h3 className="text-2xl font-medium">Customers</h3>
+        <div className="flex flex-row gap-4">
+          <button className="bg-green-500 hover:bg-transparent text-white hover:text-green-500 border border-green-500 px-2 py-1 rounded-md">
+            Add Customer
+          </button>
+          <button className="bg-blue-500 hover:bg-transparent text-white hover:text-blue-500 border border-blue-500 px-2 py-1 rounded-md">
+            Import
+          </button>
+          <button className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md">
+            Export
+          </button>
+          <input
+            type="text"
+            placeholder="Search customers..."
+            className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+          />
+        </div>
+      </div>
+      <div className="my-6 w-full flex flex-row justify-between gap-4">
+        <KpiCard
+          title="Total Customers"
+          icon={<FaUserGroup className="mr-2 text-xl text-blue-500" />}
+          value="+ 10,000"
+        />
+        <KpiCard
+          title="New This Month"
+          icon={<FaPlusSquare className="mr-2 text-xl text-green-500" />}
+          value="+ 10,000"
+        />
+        <KpiCard
+          title="Outstanding Receivables"
+          icon={<FaMoneyBill className="mr-2 text-xl text-orange-500" />}
+          value="$ 10,000"
+        />
+        <KpiCard
+          title="Active Customers"
+          icon={<FaCheckSquare className="mr-2 text-xl text-green-500" />}
+          value="+ 10,000"
+        />
+      </div>
+      <div className="bg-white rounded-2xl shadow-md p-6">
+        <div className="my-6 flex flex-row justify-start items-center gap-4">
+          <div className="flex flex-col">
+            <label className="mb-1 text-sm text-gray-600">Type</label>
+            <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
+              <option>Retail</option>
+              <option>Wholesale</option>
+              <option>Distributor</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-1 text-sm text-gray-600">Status</label>
+            <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
+              <option>Active</option>
+              <option>Inactive</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-1 text-sm text-gray-600">
+              Outstanding Balance
+            </label>
+            <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
+              <option>Has Due</option>
+              <option>No Due</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-1 text-sm text-gray-600">Location</label>
+            <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
+              <option>All</option>
+              <option>District</option>
+              <option>Division</option>
+            </select>
+          </div>
+        </div>
+        <table className="w-full text-left">
+          <thead>
+            <tr className="border-b border-gray-300 text-gray-500">
+              <th className="px-2 py-2 font-normal">Customer ID</th>
+              <th className="px-2 py-2 font-normal">Name</th>
+              <th className="px-2 py-2 font-normal">Phone</th>
+              <th className="px-2 py-2 font-normal">Email</th>
+              <th className="px-2 py-2 font-normal">Type</th>
+              <th className="px-2 py-2 font-normal">Total Orders</th>
+              <th className="px-2 py-2 font-normal">Total Spend</th>
+              <th className="px-2 py-2 font-normal">Outstanding</th>
+              <th className="px-2 py-2 font-normal">Last Purchase</th>
+              <th className="px-2 py-2 font-normal text-center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ordersData.map((item, index) => (
+              <tr
+                key={item.id}
+                className="border-b border-gray-300 hover:bg-gray-50"
+              >
+                <td className="px-2 py-4">CUST-{index}</td>
+                <td className="px-2 py-4">{item.customerName}</td>
+                <td className="px-2 py-4">+880-1122-334455</td>
+                <td className="px-2 py-4">supplier@gmail.com</td>
+                <td className="px-2 py-4">
+                  {index % 2 == 0 ? "Retail" : "Wholesale"}
+                </td>
+                <td className="px-2 py-4">{item.totalAmount / (index + 1)}</td>
+                <td className="px-2 py-4">{item.totalAmount}</td>
+                <td className="px-2 py-4">{1000 - item.totalAmount}</td>
+                <td className="px-2 py-4">12 Aug, 2025</td>
+                <td className="px-2 py-4 flex flex-wrap justify-center gap-4">
+                  <Link
+                    href={"/"}
+                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    href={"/"}
+                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    href={"/"}
+                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
+}
