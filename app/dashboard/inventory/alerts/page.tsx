84c0cf8c8411aa001bfd39678ce29e8f2ab1913a
Kawshik Biswas <kawshikbss@gmail.com>
2025-10-14 19:20:50 +0600

    fixed responsive

--- app/dashboard/inventory/alerts/page.tsx
+++ app/dashboard/inventory/alerts/page.tsx
@@ -22,6 +22,8 @@ import {
 } from "chart.js";
 import { GrPieChart } from "react-icons/gr";
 import KpiCard from "@/components/ui/KpiCard";
+import { StockAlertsTable } from "./components/StockAlertsTable";
+import { StockAlertsList } from "./components/StockAlertsList/StockAlertsList";
 ChartJS.register(
   ArcElement,
   Tooltip,
@@ -102,7 +104,7 @@ const lineChartData = {
 
 export default function LowStockAlerts() {
   return (
-    <main className="p-8">
+    <main className="p-4 md:p-8">
       <h3 className="text-2xl font-medium my-6">Stock Levels</h3>
       <div className="flex flex-row items-center justify-between mb-6 gap-4 flex-wrap">
         <div className="flex flex-row items-center gap-4 flex-wrap">
@@ -152,7 +154,7 @@ export default function LowStockAlerts() {
         </div>
       </div>
 
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Low Stock"
           icon={<PiWarningFill className="mr-2 text-xl text-green-500" />}
@@ -179,101 +181,10 @@ export default function LowStockAlerts() {
         />
       </div>
 
-      <div className="w-full bg-white rounded-2xl shadow-md p-6">
-        <h4 className="text-lg font-semibold mb-4">Stock Details</h4>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th colSpan={3} className="px-2 py-2 font-normal text-center">
-                Product
-              </th>
-              <th className="px-2 py-2 font-normal">Current Stock</th>
-              <th className="px-2 py-2 font-normal">Low Stock Threshold</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal">Last Stock In</th>
-              <th className="px-2 py-2 font-normal">Supplier</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productsData.map((product, index) => (
-              <tr
-                key={product.id}
-                className="border-b border-gray-300"
-                style={{
-                  backgroundColor:
-                    index % 3 == 0
-                      ? "#fef3c7"
-                      : index % 2 == 0
-                      ? "#fecaca"
-                      : "#fed7aa",
-                }}
-              >
-                <td className="px-2 py-4">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    <Image
-                      src={product.image}
-                      alt={product.name}
-                      width={60}
-                      height={40}
-                      className="aspect-3/2 object-cover rounded-lg"
-                    />
-                  </Link>
-                </td>
-                <td className="px-2 py-4 font-medium">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    {product.name} [{product.sku}]
-                  </Link>
-                </td>
-                <td className="px-2 py-4 font-medium">{product.category}</td>
-                <td className="px-2 py-4">{product.stockQty}</td>
-                <td className="px-2 py-4">{100 - product.stockQty}</td>
-                <td
-                  className={`px-2 py-4 text-${
-                    index % 3 == 0
-                      ? "yellow"
-                      : index % 2 == 0
-                      ? "red"
-                      : "orange"
-                  }-500`}
-                >
-                  <span className="bg-white px-2 py-1 rounded-full text-sm font-medium">
-                    {index % 3 == 0
-                      ? "Low"
-                      : index % 2 == 0
-                      ? "Out"
-                      : "Critical"}
-                  </span>
-                </td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">Supplier Name</td>
-                <td className="px-2 py-4 flex justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-blue-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Reorder
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-yellow-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Snooze
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-green-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Resolved
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <StockAlertsTable data={productsData} />
+      <StockAlertsList data={productsData} />
 
-      <div className="w-full my-6 flex flex-row justify-between items-start gap-6">
+      <div className="w-full my-6 flex flex-col md:flex-row justify-between items-start gap-6">
         <div className="w-full h-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartBar className="mr-6 text-xl" />
@@ -281,7 +192,7 @@ export default function LowStockAlerts() {
           </div>
           <Bar options={barChartOptions} data={barChartData} />
         </div>
-        <div className="w-1/3 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+        <div className="w-full md:w-1/3 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <GrPieChart className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">Stock Distribution</h4>
