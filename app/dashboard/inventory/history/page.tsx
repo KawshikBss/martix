1f4146817e7f1880630bce79ccc9c500a31011cd
Kawshik Biswas <kawshikbss@gmail.com>
2025-10-14 20:03:10 +0600

    fixed responsiveness

--- app/dashboard/inventory/history/page.tsx
+++ app/dashboard/inventory/history/page.tsx
@@ -28,6 +28,8 @@ import {
 import { GrPieChart } from "react-icons/gr";
 import { FaMoneyBill1Wave, FaPerson } from "react-icons/fa6";
 import KpiCard from "@/components/ui/KpiCard";
+import { StockAdjustmentsHistoryTable } from "./components/StockAdjustmentsHistoryTable";
+import { StockAdjustmentsHistoryList } from "./components/StockAdjustmentsHistoryList/StockAdjustmentsHistoryList";
 ChartJS.register(
   ArcElement,
   Tooltip,
@@ -108,7 +110,7 @@ const lineChartData = {
 
 export default function AdjustmentsHistory() {
   return (
-    <main className="p-8">
+    <main className="p-4 md:p-8">
       <h3 className="text-2xl font-medium my-6">Adjustments History</h3>
       <div className="flex flex-row items-center justify-between mb-6 gap-4 flex-wrap">
         <div className="flex flex-row items-center gap-4 flex-wrap">
@@ -152,7 +154,7 @@ export default function AdjustmentsHistory() {
         </div>
       </div>
 
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Adjusted"
           icon={<FaChartBar className="mr-2 text-xl text-green-500" />}
@@ -179,80 +181,10 @@ export default function AdjustmentsHistory() {
         />
       </div>
 
-      <div className="w-full bg-white rounded-2xl shadow-md p-6">
-        <h4 className="text-lg font-semibold mb-4">Adjustment Details</h4>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Date & Time</th>
-              <th colSpan={3} className="px-2 py-2 font-normal text-center">
-                Product
-              </th>
-              <th className="px-2 py-2 font-normal">Adjustment Type</th>
-              <th className="px-2 py-2 font-normal">Quantity</th>
-              <th className="px-2 py-2 font-normal">Value</th>
-              <th className="px-2 py-2 font-normal">Notes</th>
-              <th className="px-2 py-2 font-normal">Entered By</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productsData.map((product, index) => (
-              <tr key={product.id} className="border-b border-gray-300">
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    <Image
-                      src={product.image}
-                      alt={product.name}
-                      width={60}
-                      height={40}
-                      className="aspect-3/2 object-cover rounded-lg"
-                    />
-                  </Link>
-                </td>
-                <td className="px-2 py-4 font-medium">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    {product.name} [{product.sku}]
-                  </Link>
-                </td>
-                <td className="px-2 py-4 font-medium">{product.category}</td>
-                <td className="px-2 py-4">
-                  {index % 2 == 0 ? "Damaged" : "Theft"}
-                </td>
-                <td className="px-2 py-4">{product.stockQty}</td>
-                <td className="px-2 py-4">
-                  {product.stockQty * product.price}
-                </td>
-                <td className="px-2 py-4">Notes.....</td>
-                <td className="px-2 py-4">Staff Name</td>
-                <td className="px-2 py-4 flex justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Delete
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <StockAdjustmentsHistoryTable data={productsData} />
+      <StockAdjustmentsHistoryList data={productsData} />
 
-      <div className="w-full my-6 flex flex-row justify-between items-start gap-6">
+      <div className="w-full my-6 flex flex-col md:flex-row justify-between items-start gap-6">
         <div className="w-full h-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartBar className="mr-6 text-xl" />
@@ -260,7 +192,7 @@ export default function AdjustmentsHistory() {
           </div>
           <Bar options={barChartOptions} data={barChartData} />
         </div>
-        <div className="w-4/7 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+        <div className="w-full md:w-4/7 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <GrPieChart className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">Adjustment Types</h4>
