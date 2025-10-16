be0932c80e65da46811b84486d92977f91d8c5f0
Kawshik Biswas <kawshikbss@gmail.com>
2025-10-16 20:23:31 +0600

    fixed responsive

--- app/dashboard/orders/pending/page.tsx
+++ app/dashboard/orders/pending/page.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import * as React from "react";
 import { FaBox, FaHourglassHalf, FaTruck } from "react-icons/fa";
 import { FaMoneyBill1Wave } from "react-icons/fa6";
+import { PendingOrdersTable } from "./components/PendingOrdersTable";
+import { PendingOrdersList } from "./components/PendingOrdersList/PendingOrdersList";
 
 export default function PendingOrders() {
   return (
-    <main className="p-8">
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+    <main className="p-4 md:p-8">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Pending"
           icon={<FaHourglassHalf className="text-xl text-yellow-500" />}
@@ -40,8 +42,8 @@ export default function PendingOrders() {
             + New Order
           </Link>
         </div>
-        <div className="my-6 flex flex-row justify-between">
-          <div className="flex flex-row gap-4">
+        <div className="my-6 flex flex-col md:flex-row gap-4 md:gap-0 justify-between">
+          <div className="flex flex-row gap-4 flex-wrap md:flex-nowrap">
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Today</option>
               <option>Last 7 days</option>
@@ -77,79 +79,11 @@ export default function PendingOrders() {
           <input
             type="text"
             placeholder="Search products..."
-            className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+            className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
           />
         </div>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Order ID</th>
-              <th className="px-2 py-2 font-normal">Customer / Vendor</th>
-              <th className="px-2 py-2 font-normal">Date & Time</th>
-              <th className="px-2 py-2 font-normal">Items</th>
-              <th className="px-2 py-2 font-normal">Total</th>
-              <th className="px-2 py-2 font-normal">Payment</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal">Expected Delivery Date</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productsData.map((product, index) => (
-              <tr
-                key={product.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">{product.sku}</td>
-                <td className="px-2 py-4">
-                  {index % 2 == 0 ? "Customer" : "Vendor"} Name
-                </td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">{product.stockQty} items</td>
-                <td className="px-2 py-4">
-                  {product.price * product.stockQty}
-                </td>
-                <td className="px-2 py-4">{index < 2 ? "Paid" : "Unpaid"}</td>
-                <td className="px-2 py-4">
-                  {index < 2 ? "Pending" : "Completed"}
-                </td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4 flex flex-wrap justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Approve
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Mark Paid
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Ship
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Cancel
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <PendingOrdersTable data={productsData} />
+        <PendingOrdersList data={productsData} />
       </div>
     </main>
   );
