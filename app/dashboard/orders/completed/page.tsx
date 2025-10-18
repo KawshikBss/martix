0fcbfa70620fcfa2410feb8cb4775ad32506b7ff
Kawshik Biswas <kawshikbss@gmail.com>
2025-10-18 13:17:39 +0600

    fixed responsive

--- app/dashboard/orders/completed/page.tsx
+++ app/dashboard/orders/completed/page.tsx
@@ -11,11 +11,13 @@ import {
   FaTruck,
 } from "react-icons/fa";
 import { FaMoneyBill1Wave, FaPerson } from "react-icons/fa6";
+import { CompletedOrdersTable } from "./components/CompletedOrdersTable";
+import { CompletedOrdersList } from "./components/CompletedOrdersList/CompletedOrdersList";
 
 export default function CompletedOrders() {
   return (
-    <main className="p-8">
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+    <main className="p-4 md:p-8">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Completed"
           icon={<FaCheck className="text-xl text-blue-500" />}
@@ -55,8 +57,8 @@ export default function CompletedOrders() {
             + New Order
           </Link>
         </div>
-        <div className="my-6 flex flex-row justify-between">
-          <div className="flex flex-row gap-4">
+        <div className="my-6 flex flex-col md:flex-row gap-4 md:gap-0 justify-between">
+          <div className="flex flex-row flex-wrap gap-4">
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Today</option>
               <option>Last 7 days</option>
@@ -85,58 +87,12 @@ export default function CompletedOrders() {
           <input
             type="text"
             placeholder="Search products..."
-            className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+            className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
           />
         </div>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Order ID</th>
-              <th className="px-2 py-2 font-normal">Customer / Vendor</th>
-              <th className="px-2 py-2 font-normal">Date & Time</th>
-              <th className="px-2 py-2 font-normal">Items</th>
-              <th className="px-2 py-2 font-normal">Total</th>
-              <th className="px-2 py-2 font-normal">Payment</th>
-              <th className="px-2 py-2 font-normal">Delivery Method</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productsData.map((product, index) => (
-              <tr
-                key={product.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">{product.sku}</td>
-                <td className="px-2 py-4">
-                  {index % 2 == 0 ? "Customer" : "Vendor"} Name
-                </td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">{product.stockQty} items</td>
-                <td className="px-2 py-4">
-                  {product.price * product.stockQty}
-                </td>
-                <td className="px-2 py-4">{index < 2 ? "Paid" : "Unpaid"}</td>
-                <td className="px-2 py-4">Santa Claus</td>
-                <td className="px-2 py-4 flex flex-wrap justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Invoice
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <CompletedOrdersTable data={productsData} />
       </div>
+      <CompletedOrdersList data={productsData} />
     </main>
   );
 }
