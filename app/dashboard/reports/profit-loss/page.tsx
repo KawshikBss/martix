3580c7e3e7afa68413297ae6a1fead3647157d31
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-19 18:09:32 +0600

    fixed responsive

--- app/dashboard/reports/profit-loss/page.tsx
+++ app/dashboard/reports/profit-loss/page.tsx
@@ -29,6 +29,8 @@ import {
 import categoriesData from "@/public/data/categoriesData";
 import { BiLineChart, BiLineChartDown } from "react-icons/bi";
 import KpiCard from "@/components/ui/KpiCard";
+import { ProfitLossReportsTable } from "./components/ProfitLossReportsTable";
+import { ProfitLossReportList } from "./components/ProfitLossReportList/ProfitLossReportList";
 ChartJS.register(
   ArcElement,
   Tooltip,
@@ -119,7 +121,7 @@ const lineChartData = {
 
 export default function ProfitLossReport() {
   return (
-    <main className="p-8">
+    <main className="p-4 md:p-8">
       <div className="bg-white rounded-2xl shadow-md p-6">
         <div className="w-full flex flex-row justify-between items-center">
           <h3 className="text-2xl font-medium">Profit & Loss</h3>
@@ -128,7 +130,7 @@ export default function ProfitLossReport() {
           </button>
         </div>
         <div className="mt-4 flex flex-row justify-between">
-          <div className="flex flex-row gap-4">
+          <div className="flex flex-row flex-wrap gap-4">
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Today</option>
               <option>Last 7 days</option>
@@ -156,7 +158,7 @@ export default function ProfitLossReport() {
           </div>
         </div>
       </div>
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Sales"
           icon={<FaMoneyBill1Wave className="text-xl text-green-500" />}
@@ -198,67 +200,19 @@ export default function ProfitLossReport() {
           placeholder="Search products..."
           className="border border-gray-300 rounded-md px-2 py-1 my-4 w-full"
         />
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th colSpan={2} className="px-2 py-2 font-normal text-center">
-                Product
-              </th>
-              <th className="px-2 py-2 font-normal">Units Sold</th>
-              <th className="px-2 py-2 font-normal">Revenue</th>
-              <th className="px-2 py-2 font-normal">COGS</th>
-              <th className="px-2 py-2 font-normal">Profit</th>
-              <th className="px-2 py-2 font-normal">Margin</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productsData.map((product, index) => (
-              <tr
-                key={product.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    <Image
-                      src={product.image}
-                      alt={product.name}
-                      width={60}
-                      height={40}
-                      className="aspect-3/2 object-cover rounded-lg"
-                    />
-                  </Link>
-                </td>
-                <td className="px-2 py-4 font-medium">
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    {product.name}
-                  </Link>
-                </td>
-                <td className="px-2 py-4">{product.stockQty}</td>
-                <td className="px-2 py-4">
-                  {product.price * product.stockQty}
-                </td>
-                <td className="px-2 py-4">
-                  {product.price * product.stockQty}
-                </td>
-                <td className="px-2 py-4">
-                  {product.price * product.stockQty}
-                </td>
-                <td className="px-2 py-4">51%</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ProfitLossReportsTable data={productsData} />
+        <ProfitLossReportList data={productsData} />
       </div>
 
-      <div className="w-full my-6 flex flex-row justify-between items-start gap-6">
-        <div className="w-1/2 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+      <div className="w-full my-6 flex flex-col md:flex-row justify-between items-start gap-6">
+        <div className="w-full md:w-1/2 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartBar className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">Profit Trend</h4>
           </div>
           <Bar options={barChartOptions} data={barChartData} />
         </div>
-        <div className="w-1/2 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+        <div className="w-full md:w-1/2 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartLine className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">Profit by Category</h4>
@@ -267,8 +221,8 @@ export default function ProfitLossReport() {
         </div>
       </div>
 
-      <div className="w-full my-6 flex flex-row justify-between items-start gap-6">
-        <div className="w-5/7 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+      <div className="w-full my-6 flex flex-col md:flex-row justify-between items-start gap-6">
+        <div className="w-full md:w-5/7 bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartBar className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">Top 10 Products by Profit</h4>
@@ -278,7 +232,7 @@ export default function ProfitLossReport() {
             data={barChartData}
           />
         </div>
-        <div className="w-2/7 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+        <div className="w-full md:w-2/7 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <GrPieChart className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">
