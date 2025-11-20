6d2ebdde7eab059b3073bc88714b72e17ba21e4a
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-20 14:15:41 +0600

    fixed responsive

--- app/dashboard/suppliers/payments/page.tsx
+++ app/dashboard/suppliers/payments/page.tsx
@@ -14,13 +14,15 @@ import {
   FaScaleBalanced,
   FaUserGroup,
 } from "react-icons/fa6";
+import { SupplierPaymentsList } from "./components/SupplierPaymentsList/SupplierPaymentsList";
+import { SupplierPaymentsTable } from "./components/SupplierPaymentsTable";
 
 export default function SupplierPayments() {
   return (
-    <main className="p-8">
-      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-row justify-between items-center">
-        <h3 className="text-2xl font-medium">Supplier Payments</h3>
-        <div className="flex flex-row gap-4">
+    <main className="p-4 md:p-8">
+      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0">
+        <h3 className="text-2xl font-medium self-start">Supplier Payments</h3>
+        <div className="flex flex-row flex-wrap gap-4">
           <button className="bg-green-500 hover:bg-transparent text-white hover:text-green-500 border border-green-500 px-2 py-1 rounded-md">
             Record Payment
           </button>
@@ -33,11 +35,11 @@ export default function SupplierPayments() {
           <input
             type="text"
             placeholder="Search payments..."
-            className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+            className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
           />
         </div>
       </div>
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Paid This Month"
           icon={<FaMoneyBill1Wave className="mr-2 text-xl text-green-500" />}
@@ -60,8 +62,8 @@ export default function SupplierPayments() {
         />
       </div>
       <div className="bg-white rounded-2xl shadow-md p-6">
-        <div className="my-6 flex flex-row justify-start items-center gap-4">
-          <div className="flex flex-col">
+        <div className="md:my-6 flex flex-row justify-start items-center gap-4">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">Supplier</label>
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Supplier 1</option>
@@ -70,7 +72,7 @@ export default function SupplierPayments() {
               <option>Supplier 4</option>
             </select>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">Status</label>
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Pending</option>
@@ -79,61 +81,9 @@ export default function SupplierPayments() {
             </select>
           </div>
         </div>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Payment ID</th>
-              <th className="px-2 py-2 font-normal">Date</th>
-              <th className="px-2 py-2 font-normal">Supplier</th>
-              <th className="px-2 py-2 font-normal">Related PO</th>
-              <th className="px-2 py-2 font-normal">Amount</th>
-              <th className="px-2 py-2 font-normal">Payment Method</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersData.map((item, index) => (
-              <tr
-                key={item.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">PAY-{index}</td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">{item.customerName}</td>
-                <td className="px-2 py-4">PO-{index}</td>
-                <td className="px-2 py-4">{item.totalAmount}</td>
-                <td className="px-2 py-4">
-                  {index % 2 === 0 ? "Card" : "Cash"}
-                </td>
-                <td className="px-2 py-4">
-                  {index % 2 === 0 ? "Cleared" : "Partial"}
-                </td>
-                <td className="px-2 py-4 flex flex-wrap justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Delete
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <SupplierPaymentsTable data={ordersData} />
       </div>
+      <SupplierPaymentsList data={ordersData} />
     </main>
   );
 }
