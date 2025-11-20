52cb319e7c8028cd7a33eb25a705e864d6a8412f
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-20 17:30:17 +0600

    fixed responsive

--- app/dashboard/stores/transfer/page.tsx
+++ app/dashboard/stores/transfer/page.tsx
@@ -3,17 +3,19 @@ import ordersData from "@/public/data/ordersData";
 import Link from "next/link";
 import * as React from "react";
 import { FaBan, FaCheckSquare, FaHourglassHalf, FaTruck } from "react-icons/fa";
+import { TransferList } from "./components/TransferList/TransferList";
+import { TransferTable } from "./components/TransferTable";
 
 export default function TransferStocks() {
   return (
-    <main className="p-8">
-      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-row justify-between items-center">
+    <main className="p-4 md:p-8">
+      <div className="bg-white rounded-2xl shadow-md p-4 md:p-6 w-full flex flex-row justify-between items-center">
         <h3 className="text-2xl font-medium">Transfer Stock</h3>
         <button className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md">
           New Transfer
         </button>
       </div>
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="In Transit"
           icon={<FaTruck className="mr-2 text-xl text-blue-500" />}
@@ -38,71 +40,11 @@ export default function TransferStocks() {
       <div className="bg-white rounded-2xl shadow-md p-6">
         <input
           type="text"
-          placeholder="Search products..."
-          className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+          placeholder="Search transfers..."
+          className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
         />
-        <table className="w-full text-left mt-4">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Transfer ID</th>
-              <th className="px-2 py-2 font-normal">From Location</th>
-              <th className="px-2 py-2 font-normal">To Location</th>
-              <th className="px-2 py-2 font-normal">Items Count</th>
-              <th className="px-2 py-2 font-normal">Total Quantity</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal">Requested By</th>
-              <th className="px-2 py-2 font-normal">Date Initiated</th>
-              <th className="px-2 py-2 font-normal">Last Updated</th>
-              <th className="px-2 py-2 font-normal">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersData.map((item, index) => (
-              <tr
-                key={item.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">TRF-{index}</td>
-                <td className="px-2 py-4">Location {index}</td>
-                <td className="px-2 py-4">Location {index + 1}</td>
-                <td className="px-2 py-4">{item.totalAmount / 10}</td>
-                <td className="px-2 py-4">{item.totalAmount}</td>
-                <td className="px-2 py-4">
-                  {index % 2 == 0 ? "Pending" : "Completed"}
-                </td>
-                <td className="px-2 py-4">Staff {index + 1}</td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">12 Sep, 2025</td>
-                <td className="px-2 py-4 flex justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Approve
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Cancel
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Print
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <TransferTable data={ordersData} />
+        <TransferList data={ordersData} />
       </div>
     </main>
   );
