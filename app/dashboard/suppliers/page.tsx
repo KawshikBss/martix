5bafaae176f46549dd33870730e6ef5a9038a95e
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-20 13:32:35 +0600

    fixed responsive

--- app/dashboard/suppliers/page.tsx
+++ app/dashboard/suppliers/page.tsx
@@ -5,13 +5,15 @@ import productsData from "@/public/data/productsData";
 import Link from "next/link";
 import * as React from "react";
 import { FaCheckSquare, FaMoneyBill, FaTrophy, FaTruck } from "react-icons/fa";
+import { SuppliersList } from "./components/SuppliersList/SuppliersList";
+import { SuppliersTable } from "./components/SuppliersTable";
 
 export default function AllSuppliers() {
   return (
-    <main className="p-8">
-      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-row justify-between items-center">
-        <h3 className="text-2xl font-medium">Sales Reports</h3>
-        <div className="flex flex-row gap-4">
+    <main className="p-4 md:p-8">
+      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0">
+        <h3 className="text-2xl font-medium self-start">Suppliers</h3>
+        <div className="flex flex-row flex-wrap gap-4">
           <button className="bg-green-500 hover:bg-transparent text-white hover:text-green-500 border border-green-500 px-2 py-1 rounded-md">
             Add Supplier
           </button>
@@ -24,11 +26,11 @@ export default function AllSuppliers() {
           <input
             type="text"
             placeholder="Search suppliers..."
-            className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+            className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
           />
         </div>
       </div>
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Suppliers"
           icon={<FaTruck className="mr-2 text-xl text-blue-500" />}
@@ -55,15 +57,15 @@ export default function AllSuppliers() {
         />
       </div>
       <div className="bg-white rounded-2xl shadow-md p-6">
-        <div className="my-6 flex flex-row justify-start items-center gap-4">
-          <div className="flex flex-col">
+        <div className="my-6 flex flex-col md:flex-row justify-start items-center gap-4">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">Status</label>
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>Active</option>
               <option>Inactive</option>
             </select>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">Category</label>
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>All</option>
@@ -72,7 +74,7 @@ export default function AllSuppliers() {
               ))}
             </select>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">Location</label>
             <select className="bg-white border border-gray-300 rounded-md px-2 py-2">
               <option>All</option>
@@ -80,7 +82,7 @@ export default function AllSuppliers() {
               <option>Division</option>
             </select>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full md:w-fit">
             <label className="mb-1 text-sm text-gray-600">
               Outstanding Balance (Above)
             </label>
@@ -92,61 +94,9 @@ export default function AllSuppliers() {
             />
           </div>
         </div>
-        <table className="w-full text-left">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Supplier</th>
-              <th className="px-2 py-2 font-normal">Contact Person</th>
-              <th className="px-2 py-2 font-normal">Phone</th>
-              <th className="px-2 py-2 font-normal">Email</th>
-              <th className="px-2 py-2 font-normal">Total Purchases</th>
-              <th className="px-2 py-2 font-normal">Pending Payment</th>
-              <th className="px-2 py-2 font-normal">Last Purchase</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersData.map((item, index) => (
-              <tr
-                key={item.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">{item.customerName}</td>
-                <td className="px-2 py-4">{item.customerName}</td>
-                <td className="px-2 py-4">+880-1122-334455</td>
-                <td className="px-2 py-4">supplier@gmail.com</td>
-                <td className="px-2 py-4">{item.totalAmount}</td>
-                <td className="px-2 py-4">{1000 - item.totalAmount}</td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-                <td className="px-2 py-4">
-                  {index % 2 === 0 ? "Active" : "Inactive"}
-                </td>
-                <td className="px-2 py-4 flex flex-wrap justify-center gap-4">
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    View
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-                  >
-                    Disable
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <SuppliersTable data={ordersData} />
       </div>
+      <SuppliersList data={productsData} />
     </main>
   );
 }
