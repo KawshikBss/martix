939b462ccd3d8e007895333cba5db0c535aa21a5
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-20 13:32:09 +0600

    exported interface

--- public/data/ordersData.tsx
+++ public/data/ordersData.tsx
@@ -1,29 +1,29 @@
-interface Order {
-    id: number;
-    customerName: string;
-    totalAmount: number;
-    status: 0 | 1 | 2;
+export interface Order {
+  id: number;
+  customerName: string;
+  totalAmount: number;
+  status: 0 | 1 | 2;
 }
 
 const ordersData: Order[] = [
-    {
-        id: 1,
-        customerName: "John Doe",
-        totalAmount: 100,
-        status: 1,
-    },
-    {
-        id: 2,
-        customerName: "Jane Smith",
-        totalAmount: 200,
-        status: 0,
-    },
-    {
-        id: 3,
-        customerName: "Alice Johnson",
-        totalAmount: 150,
-        status: 2,
-    },
+  {
+    id: 1,
+    customerName: "John Doe",
+    totalAmount: 100,
+    status: 1,
+  },
+  {
+    id: 2,
+    customerName: "Jane Smith",
+    totalAmount: 200,
+    status: 0,
+  },
+  {
+    id: 3,
+    customerName: "Alice Johnson",
+    totalAmount: 150,
+    status: 2,
+  },
 ];
 
 export default ordersData;
