76a86b0f175e70d7d58a26fe86ddcdf34ca06fa6
Kawshik Biswas <kawshikbss@gmail.com>
2025-11-20 17:18:29 +0600

    fixed responsive

--- app/dashboard/stores/page.tsx
+++ app/dashboard/stores/page.tsx
@@ -24,6 +24,8 @@ import {
   LineElement,
 } from "chart.js";
 import { GrPieChart } from "react-icons/gr";
+import { StoresList } from "./components/StoresList/StoresList";
+import { StoresTable } from "./components/StoresTable";
 ChartJS.register(
   ArcElement,
   PointElement,
@@ -118,14 +120,14 @@ const lineChartData = {
 
 export default function BranchOverview() {
   return (
-    <main className="p-8">
-      <div className="bg-white rounded-2xl shadow-md p-6 w-full flex flex-row justify-between items-center">
+    <main className="p-4 md:p-8">
+      <div className="bg-white rounded-2xl shadow-md p-4 md:p-6 w-full flex flex-row justify-between items-center">
         <h3 className="text-2xl font-medium">Branch Overview</h3>
         <button className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md">
           Add Branch
         </button>
       </div>
-      <div className="my-6 w-full flex flex-row justify-between gap-4">
+      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
         <KpiCard
           title="Total Branches"
           icon={<FaStore className="mr-2 text-xl text-blue-500" />}
@@ -155,46 +157,13 @@ export default function BranchOverview() {
       <div className="bg-white rounded-2xl shadow-md p-6">
         <input
           type="text"
-          placeholder="Search products..."
-          className="border border-gray-300 rounded-md px-2 py-1 w-2/5"
+          placeholder="Search stores..."
+          className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
         />
-        <table className="w-full text-left mt-4">
-          <thead>
-            <tr className="border-b border-gray-300 text-gray-500">
-              <th className="px-2 py-2 font-normal">Branch Name</th>
-              <th className="px-2 py-2 font-normal">Manager Name</th>
-              <th className="px-2 py-2 font-normal">Location</th>
-              <th className="px-2 py-2 font-normal">Status</th>
-              <th className="px-2 py-2 font-normal">Today’s Sales</th>
-              <th className="px-2 py-2 font-normal">Monthly Sales</th>
-              <th className="px-2 py-2 font-normal">Inventory Value</th>
-              <th className="px-2 py-2 font-normal">Low Stock Items</th>
-              <th className="px-2 py-2 font-normal">Last Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersData.map((item, index) => (
-              <tr
-                key={item.id}
-                className="border-b border-gray-300 hover:bg-gray-50"
-              >
-                <td className="px-2 py-4">BRCH-{index}</td>
-                <td className="px-2 py-4">{item.customerName}</td>
-                <td className="px-2 py-4">Location</td>
-                <td className="px-2 py-4">
-                  {index % 2 == 0 ? "Active" : "Inactive"}
-                </td>
-                <td className="px-2 py-4">{item.totalAmount}</td>
-                <td className="px-2 py-4">{1000 + item.totalAmount}</td>
-                <td className="px-2 py-4">{2000 + item.totalAmount}</td>
-                <td className="px-2 py-4">{(index + 1) * 20}</td>
-                <td className="px-2 py-4">12 Aug, 2025</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <StoresTable data={ordersData} />
+        <StoresList data={ordersData} />
       </div>
-      <div className="w-full my-6 flex flex-row justify-between items-start gap-6">
+      <div className="w-full my-6 flex flex-col md:flex-row justify-between items-start gap-6">
         <div className="w-full h-full bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <FaChartBar className="mr-6 text-xl" />
@@ -202,7 +171,7 @@ export default function BranchOverview() {
           </div>
           <Bar options={barChartOptions} data={barChartData} />
         </div>
-        <div className="w-1/3 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
+        <div className="w-full md:w-1/3 aspect-square bg-white rounded-2xl shadow-md p-4 flex flex-col justify-between">
           <div className="mb-4 flex flex-row justify-start items-center">
             <GrPieChart className="mr-6 text-xl" />
             <h4 className="text-lg font-semibold">
