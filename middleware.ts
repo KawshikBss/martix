4f5308abfff059cbe980af183ca5e9d88b3cedb9
KawshikBss <kawshikbss@gmail.com>
2025-12-11 01:49:46 +0600

    fixed auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,17 @@
+import { getCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const authToken = request.cookies.get("authToken");
-  return NextResponse.redirect(new URL("/", request.url));
+    const authToken = request.cookies.get("authToken")?.value;
+    if (authToken) {
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/login", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/dashboar/:path*",
+    matcher: "/dashboard/:path*",
 };
