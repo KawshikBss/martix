644163d5c62c8d969701706bcdbabca27c9b67a4
KawshikBss <kawshikbss@gmail.com>
2025-12-11 03:07:51 +0600

    moved useAuth inside hooks

--- lib/hooks/useAuth.ts
+++ lib/hooks/useAuth.ts
@@ -1,17 +1,26 @@
 import { deleteCookie, getCookie, setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 const useAuth = () => {
-    const login = async (authToken: string) => {
+    const { replace } = useRouter();
+    const login = async (authToken: string, authUser: object) => {
         console.log("Setting auth token:", authToken);
         await setCookie("authToken", authToken, {
             secure: false,
             sameSite: "lax",
             path: "/",
         });
+        await setCookie("authUser", authUser, {
+            secure: false,
+            sameSite: "lax",
+            path: "/",
+        });
+        replace("/dashboard");
     };
 
     const logout = async () => {
         await deleteCookie("authToken");
+        replace("/login");
     };
 
     const isAuthenticated = async () => {
@@ -22,9 +31,15 @@ const useAuth = () => {
         return false;
     };
 
+    const getAuthUser = async () => {
+        const authUser = await getCookie("authUser");
+        return JSON.parse(authUser ?? "{}") as UserInterface;
+    };
+
     return {
         login,
         logout,
+        getAuthUser,
         isAuthenticated,
     };
 };
