e8cddeefadd93790ca239b7267cb3e05c62e62ee
KawshikBss <kawshikbss@gmail.com>
2025-12-11 19:08:56 +0600

    modified middleware

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,41 @@
-import { getCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { UserInterface } from "./lib/interfaces/UserInterface";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+    const currentPath = request.nextUrl.pathname;
     const authToken = request.cookies.get("authToken")?.value;
+
     if (authToken) {
+        if (currentPath === "/login" || currentPath == "/signup") {
+            return NextResponse.redirect(new URL("/dashboard", request.url));
+        }
         const authUserString = request.cookies.get("authUser")?.value;
         const authUser = JSON.parse(authUserString || "{}") as UserInterface;
         if (
-            (authUser &&
-                authUser.name &&
-                authUser.email &&
-                authUser.phone &&
-                authUser.image &&
-                authUser.address &&
-                authUser.nid) ||
-            request.nextUrl.pathname === "/dashboard/profile"
+            currentPath !== "/dashboard/profile" &&
+            authUser &&
+            (!authUser.name ||
+                !authUser.email ||
+                !authUser.phone ||
+                !authUser.image ||
+                !authUser.address ||
+                !authUser.nid)
         ) {
-            return NextResponse.next();
+            return NextResponse.redirect(
+                new URL("/dashboard/profile", request.url)
+            );
         }
-        return NextResponse.redirect(
-            new URL("/dashboard/profile", request.url)
-        );
+        return NextResponse.next();
+    }
+    if (currentPath === "/login" || currentPath == "/signup") {
+        return NextResponse.next();
     }
     return NextResponse.redirect(new URL("/login", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: "/dashboard/:path*",
+    matcher: ["/dashboard/:path*", "/login", "/signup"],
 };
