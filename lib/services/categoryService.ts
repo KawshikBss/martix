5c4113354cc4511ea9501ead419df3704a60558e
KawshikBss <kawshikbss@gmail.com>
2025-12-13 02:27:52 +0600

    created category service

---
+++ lib/services/categoryService.ts
@@ -0,0 +1,14 @@
+import { apiClient } from "../core/apiClient";
+import { CategoryInterface } from "../interfaces/CategoryInterface";
+
+export const categoryService = {
+    getCategories: async (): Promise<CategoryInterface[]> => {
+        return await apiClient.get("categories");
+    },
+
+    createCategory: async (
+        payload: FormData | object
+    ): Promise<CategoryInterface> => {
+        return await apiClient.post("categories", payload);
+    },
+};
