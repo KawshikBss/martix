38b569dfc07a769d4f02e97946fe2bb9bf213ebc
KawshikBss <kawshikbss@gmail.com>
2025-12-13 02:27:12 +0600

    created category interface

---
+++ lib/interfaces/CategoryInterface.ts
@@ -0,0 +1,9 @@
+export interface CategoryInterface {
+    id: string;
+    name: string;
+    slug: string;
+    image: string;
+    parent_id: string;
+    visible_stores: string[];
+    status: string;
+}
