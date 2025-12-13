539a6b0548be1c950e1ecb4b1177868344a4e807
KawshikBss <kawshikbss@gmail.com>
2025-12-13 17:20:36 +0600

    moved dynamic data to component

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -1,65 +1,24 @@
-import categoriesData from "@/public/data/categoriesData";
-import Image from "next/image";
 import Link from "next/link";
-import * as React from "react";
+import CategoryList from "./components/CategoryList/CategoryList";
 
 export default function Categories() {
-  return (
-    <main className="p-8">
-      <div className="bg-white rounded-2xl shadow-md p-6">
-        <div className="w-full flex flex-col md:flex-row justify-between items-center">
-          <h3 className="text-2xl font-medium mb-4 md:mb-0">Categories</h3>
-          <Link
-            href="/dashboard/categories/add"
-            className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md"
-          >
-            + Add New Category
-          </Link>
-        </div>
+    return (
+        <main className="p-4 md:p-8">
+            <div className="bg-white rounded-2xl shadow-md p-2 md:p-6">
+                <div className="w-full flex flex-col md:flex-row justify-between items-center">
+                    <h3 className="text-2xl font-medium mb-4 md:mb-0">
+                        Categories
+                    </h3>
+                    <Link
+                        href="/dashboard/categories/add"
+                        className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md"
+                    >
+                        + Add New Category
+                    </Link>
+                </div>
 
-        <input
-          type="text"
-          placeholder="Search categories..."
-          className="border border-gray-300 rounded-md my-4 px-2 py-1 w-full md:w-2/5"
-        />
-
-        {categoriesData.map((category) => (
-          <div key={category.slug} className="px-4 py-2 my-1">
-            <div className="flex flex-row justify-start items-center">
-              {category.image && (
-                <Image
-                  src={category.image}
-                  alt={category.name}
-                  width={20}
-                  height={20}
-                  className="rounded-md w-10 h-8 object-cover me-2"
-                />
-              )}
-              {category.name}
+                <CategoryList />
             </div>
-            <div className="flex flex-col pl-4 mt-1">
-              {category.children &&
-                category.children.map((child) => (
-                  <div
-                    key={child.slug}
-                    className="my-1 flex flex-row justify-start items-center"
-                  >
-                    {child.image && (
-                      <Image
-                        src={child.image}
-                        alt={child.name}
-                        width={20}
-                        height={20}
-                        className="rounded-md w-10 h-8 object-cover me-2"
-                      />
-                    )}
-                    {child.name}
-                  </div>
-                ))}
-            </div>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
