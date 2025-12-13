cd5204ee9c4cf49ed7b33f7ce96204aa3c40b88a
KawshikBss <kawshikbss@gmail.com>
2025-12-13 19:11:18 +0600

    create update page for category

---
+++ app/dashboard/categories/[categoryId]/edit/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import * as React from "react";
+import { toast } from "react-toastify";
+import { useParams, useRouter } from "next/navigation";
+import CategoryForm from "../../components/CategoryForm";
+import { useCategory } from "@/lib/hooks/categories/useCategory";
+import { useUpdateCategory } from "@/lib/hooks/categories/useUpdateCategory";
+
+export default function UpdateCategory() {
+    const { categoryId } = useParams();
+
+    const { data: category } = useCategory(categoryId?.toString());
+
+    const { back } = useRouter();
+
+    const { mutateAsync: updateCategoryMutation } = useUpdateCategory();
+
+    const categoryFormRef = React.useRef<HTMLFormElement>(null);
+
+    const [imagePreview, setImagePreview] = React.useState<string | null>(null);
+
+    const resetForm = () => {
+        if (!categoryFormRef.current) return;
+        categoryFormRef.current.reset();
+        setImagePreview(null);
+    };
+
+    const handleCategoryUpdate = async () => {
+        if (!categoryFormRef.current) return;
+        const formElements = categoryFormRef.current.elements;
+        const image = (formElements.namedItem("image") as HTMLInputElement)
+            .files?.[0];
+        const name = (formElements.namedItem("name") as HTMLInputElement).value;
+        const slug = (formElements.namedItem("sku") as HTMLInputElement).value;
+        const parentId = (
+            formElements.namedItem("parent_id") as HTMLInputElement
+        ).value;
+        const status = (formElements.namedItem("status") as HTMLInputElement)
+            .checked;
+
+        try {
+            const formData = new FormData();
+            if (image) formData.append("image", image);
+            formData.append("name", name);
+            formData.append("slug", slug);
+            if (parentId) formData.append("parent_id", parentId);
+            formData.append("status", status ? "active" : "inactive");
+
+            const response = await updateCategoryMutation({
+                id: category?.id,
+                payload: formData,
+            });
+            if (response) {
+                toast.success("Category updated successfully!");
+            }
+        } catch (error) {
+            console.error("Error creating category:", error);
+        }
+    };
+
+    return (
+        <main className="p-8">
+            <div className="w-full bg-white rounded-2xl shadow-md p-6 flex flex-col md:flex-row justify-between items-center">
+                <h3 className="text-2xl font-medium mb-4 md:mb-0">
+                    Update Category
+                </h3>
+                <div>
+                    <button
+                        onClick={back}
+                        className="bg-transparent hover:bg-gray-500 text-gray-500 hover:text-white border border-gray-500 px-2 py-1 rounded-md cursor-pointer mr-2"
+                    >
+                        Go Back
+                    </button>
+                    <button
+                        onClick={resetForm}
+                        className="bg-transparent hover:bg-red-500 text-red-500 hover:text-white border border-red-500 px-2 py-1 rounded-md cursor-pointer mr-2"
+                    >
+                        Discard Changes
+                    </button>
+                    <button
+                        onClick={handleCategoryUpdate}
+                        className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md cursor-pointer"
+                    >
+                        Update
+                    </button>
+                </div>
+            </div>
+            <CategoryForm
+                ref={categoryFormRef}
+                imagePreview={imagePreview ?? category?.image_url}
+                setImagePreview={setImagePreview}
+                category={category}
+            />
+        </main>
+    );
+}
