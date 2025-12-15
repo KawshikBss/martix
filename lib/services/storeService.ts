ecf29811774f747ab0b5e2847c74304aed1789d0
KawshikBss <kawshikbss@gmail.com>
2025-12-15 19:26:29 +0600

    added new function

--- lib/services/storeService.ts
+++ lib/services/storeService.ts
@@ -4,6 +4,16 @@ import { StoreInterface } from "../interfaces/StoreIntefrace";
 export const storeService = {
     getStores: async (): Promise<StoreInterface[]> =>
         await apiClient.get("stores"),
+
+    getStore: async (id?: string): Promise<StoreInterface> =>
+        await apiClient.get(`stores/${id}`),
+
     createStore: async (payload: FormData | object): Promise<StoreInterface> =>
         await apiClient.post("stores", payload),
+
+    updateStore: async (params: {
+        id?: string;
+        payload: FormData | object;
+    }): Promise<StoreInterface> =>
+        await apiClient.post(`stores/update/${params.id}`, params.payload),
 };
