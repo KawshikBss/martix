1346018d291d51664f0197378023b964751c9043
KawshikBss <kawshikbss@gmail.com>
2025-12-16 19:34:55 +0600

    made dynamic

--- app/dashboard/products/components/ProductsTable.tsx
+++ app/dashboard/products/components/ProductsTable.tsx
@@ -1,86 +1,109 @@
-import { IProduct } from "@/public/data/productsData";
+"use client";
+
+import Loader from "@/components/ui/loaders/Loader";
+import { useProducts } from "@/lib/hooks/products/useProducts";
 import Image from "next/image";
 import Link from "next/link";
-import * as React from "react";
 
-export interface IProductsTableProps {
-  items?: IProduct[];
-}
+export interface IProductsTableProps {}
 
 export function ProductsTable(props: IProductsTableProps) {
-  if (!props.items || props.items.length === 0) {
-    return (
-      <div className="text-center text-gray-500 py-10">
-        No products available.
-      </div>
-    );
-  }
+    const {
+        data: products,
+        isLoading: productsIsLoading,
+        isSuccess: productsIsSuccess,
+    } = useProducts();
 
-  return (
-    <table className="w-full text-left hidden md:table">
-      <thead>
-        <tr className="border-b border-gray-300 text-gray-500">
-          <th className="px-2 py-2">
-            <input type="checkbox" />
-          </th>
-          <th colSpan={2} className="px-2 py-2 font-normal text-center">
-            Product
-          </th>
-          <th className="px-2 py-2 font-normal">SKU / Barcode</th>
-          <th className="px-2 py-2 font-normal">Category</th>
-          <th className="px-2 py-2 font-normal">Price</th>
-          <th className="px-2 py-2 font-normal text-end">Stock Qty</th>
-          <th className="px-2 py-2 font-normal text-end">Total Sales</th>
-          <th className="px-2 py-2 font-normal text-center">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.items?.map((product) => (
-          <tr
-            key={product.id}
-            className="border-b border-gray-300 hover:bg-gray-50"
-          >
-            <td className="px-2 py-4">
-              <input type="checkbox" />
-            </td>
-            <td className="px-2 py-4">
-              <Link href={`/dashboard/products/${product.id}`}>
-                <Image
-                  src={product.image}
-                  alt={product.name}
-                  width={60}
-                  height={40}
-                  className="aspect-3/2 object-cover rounded-lg"
-                />
-              </Link>
-            </td>
-            <td className="px-2 py-4 font-medium">
-              <Link href={`/dashboard/products/${product.id}`}>
-                {product.name}
-              </Link>
-            </td>
-            <td className="px-2 py-4">{product.sku}</td>
-            <td className="px-2 py-4">{product.category}</td>
-            <td className="px-2 py-4">${product.price}</td>
-            <td className="px-2 py-4 text-end">{product.stockQty}</td>
-            <td className="px-2 py-4 text-end">120</td>
-            <td className="px-2 py-4 flex justify-center gap-4">
-              <Link
-                href={"/"}
-                className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-              >
-                Edit
-              </Link>
-              <Link
-                href={"/"}
-                className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
-              >
-                Delete
-              </Link>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return !productsIsLoading && productsIsSuccess && products.length ? (
+        <table className="w-full text-left hidden md:table">
+            <thead>
+                <tr className="border-b border-gray-300 text-gray-500">
+                    <th className="px-2 py-2">
+                        <input type="checkbox" />
+                    </th>
+                    <th
+                        colSpan={2}
+                        className="px-2 py-2 font-normal text-center"
+                    >
+                        Product
+                    </th>
+                    <th className="px-2 py-2 font-normal">SKU / Barcode</th>
+                    <th className="px-2 py-2 font-normal">Category</th>
+                    <th className="px-2 py-2 font-normal">Price</th>
+                    <th className="px-2 py-2 font-normal text-end">
+                        Stock Qty
+                    </th>
+                    <th className="px-2 py-2 font-normal text-end">
+                        Total Sales
+                    </th>
+                    <th className="px-2 py-2 font-normal text-center">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {products?.map((product) => (
+                    <tr
+                        key={product.id}
+                        className="border-b border-gray-300 hover:bg-gray-50"
+                    >
+                        <td className="px-2 py-4">
+                            <input type="checkbox" />
+                        </td>
+                        <td className="px-2 py-4">
+                            <Link href={`/dashboard/products/${product.id}`}>
+                                <Image
+                                    src={product.image_url}
+                                    alt={product.name}
+                                    width={60}
+                                    height={40}
+                                    className="aspect-3/2 object-cover rounded-lg"
+                                />
+                            </Link>
+                        </td>
+                        <td className="px-2 py-4 font-medium">
+                            <Link href={`/dashboard/products/${product.id}`}>
+                                {product.name}
+                            </Link>
+                        </td>
+                        <td className="px-2 py-4">{product.sku}</td>
+                        <td className="px-2 py-4">{product.category.name}</td>
+                        <td className="px-2 py-4">${product.cost_price}</td>
+                        <td className="px-2 py-4 text-end">10</td>
+                        <td className="px-2 py-4 text-end">120</td>
+                        <td className="px-2 py-4 flex justify-center gap-4">
+                            <Link
+                                href={`/dashboard/products/${product.id}`}
+                                className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                            >
+                                View
+                            </Link>
+                            <Link
+                                href={`/dashboard/products/${product.id}/edit`}
+                                className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                            >
+                                Edit
+                            </Link>
+                            <Link
+                                href={"/"}
+                                className="bg-gray-200 px-2 py-1 rounded-md hover:bg-white"
+                            >
+                                Delete
+                            </Link>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    ) : !productsIsLoading && productsIsSuccess && !products?.length ? (
+        <p>
+            No products yet{" "}
+            <Link href="/dashboard/products/add" className="text-[#615cf6]">
+                Add new
+            </Link>
+            ?
+        </p>
+    ) : (
+        <Loader />
+    );
 }
