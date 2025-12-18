cc895058475ea2518829b911d715ff4cc732f5db
KawshikBss <kawshikbss@gmail.com>
2025-12-19 03:10:16 +0600

    added attributes

--- lib/interfaces/InventoryInterface.ts
+++ lib/interfaces/InventoryInterface.ts
@@ -1,5 +1,12 @@
+import { StoreInterface } from "./StoreIntefrace";
+
 export interface InventoryInterface {
     id: string;
     initial_quantity: number;
     quantity: number;
+    reorder_level: number;
+    barcode: string;
+    selling_price: number;
+    expiry_date: string;
+    store: StoreInterface;
 }
