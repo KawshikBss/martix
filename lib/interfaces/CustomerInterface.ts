3d9337bb09bc01193a8a6896fce7c4a78ce22b2d
KawshikBss <kawshikbss@gmail.com>
2026-01-07 19:13:46 +0600

    created customers iinterface

---
+++ lib/interfaces/CustomerInterface.ts
@@ -0,0 +1,9 @@
+import { RoleInterface } from "./RoleInterface";
+
+export interface CustomerInterface {
+    id: string;
+    name: string;
+    phone: string;
+    email: string;
+    address: string;
+}
