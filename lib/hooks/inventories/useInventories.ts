9d8a3042dc3b88ef804f7e0ef6525be7b15ee1c9
KawshikBss <kawshikbss@gmail.com>
2026-02-14 19:50:44 +0600

    fixed

--- lib/hooks/inventories/useInventories.ts
+++ lib/hooks/inventories/useInventories.ts
@@ -1,9 +1,35 @@
 import { inventoryService } from "@/lib/services/inventoryService";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export function useInventories() {
-    return useQuery({
-        queryKey: ["inventories"],
-        queryFn: () => inventoryService.getInventories(),
+export function useInventories(params?: {
+    query?: string;
+    filters?: {
+        manager?: string;
+        branch?: string;
+        location?: string;
+        status?: string;
+        stock_level?: string;
+        type?: string;
+        min_inventory_value?: string;
+        max_inventory_value?: string;
+        has_staff?: string;
+        has_low_stock?: string;
+        has_expired_products?: string;
+        has_soon_expiring_products?: string;
+        min_create_date?: string;
+        max_create_date?: string;
+        min_update_date?: string;
+        max_update_date?: string;
+    };
+}) {
+    return useInfiniteQuery({
+        queryKey: ["inventories", params?.query, params?.filters],
+        queryFn: ({ pageParam = 1 }) =>
+            inventoryService.getInventories({ ...params, page: pageParam }),
+        getNextPageParam: (lastPage) => {
+            const { current_page, last_page } = lastPage;
+            return current_page < last_page ? current_page + 1 : undefined;
+        },
+        initialPageParam: 1,
     });
 }
--- lib/services/inventoryService.ts
+++ lib/services/inventoryService.ts
@@ -1,10 +1,38 @@
 import { apiClient } from "../core/apiClient";
+import { PaginatedResponse } from "../core/PaginatedResponse";
 import { InventoryInterface } from "../interfaces/InventoryInterface";
 import { InventoryMovementInterface } from "../interfaces/InventoryMovementInterface";
 
 export const inventoryService = {
-    getInventories: async (): Promise<InventoryInterface[]> =>
-        await apiClient.get("inventories"),
+    getInventories: async (params: {
+        query?: string;
+        page?: number;
+        filters?: {
+            manager?: string;
+            branch?: string;
+            location?: string;
+            status?: string;
+            stock_level?: string;
+            type?: string;
+            min_inventory_value?: string;
+            max_inventory_value?: string;
+            has_staff?: string;
+            has_low_stock?: string;
+            has_expired_products?: string;
+            has_soon_expiring_products?: string;
+            min_create_date?: string;
+            max_create_date?: string;
+            min_update_date?: string;
+            max_update_date?: string;
+        };
+    }): Promise<PaginatedResponse<InventoryInterface>> =>
+        await apiClient.get(
+            `inventories?page=${params?.page ?? 1}&query=${
+                params?.query ?? ""
+            }&${Object.entries(params?.filters ?? {})
+                .map((item) => (item[1].length ? `${item[0]}=${item[1]}` : ""))
+                .join("&")}`,
+        ),
     getInventoryMovements: async (): Promise<InventoryMovementInterface[]> =>
         await apiClient.get("inventories/movements"),
 };
