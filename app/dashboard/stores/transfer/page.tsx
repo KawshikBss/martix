0f2e3168a2538f8654d34c1f0589243fb0005336
KawshikBss <kawshikbss@gmail.com>
2026-02-18 16:31:42 +0600

    fixed stock transfer page

--- app/dashboard/stores/transfer/page.tsx
+++ app/dashboard/stores/transfer/page.tsx
@@ -1,51 +1,31 @@
-import KpiCard from "@/components/ui/KpiCard";
-import ordersData from "@/public/data/ordersData";
-import Link from "next/link";
-import * as React from "react";
-import { FaBan, FaCheckSquare, FaHourglassHalf, FaTruck } from "react-icons/fa";
-import { TransferList } from "./components/TransferList/TransferList";
-import { TransferTable } from "./components/TransferTable";
+"use client";
 
-export default function TransferStocks() {
-  return (
-    <main className="p-4 md:p-8">
-      <div className="bg-white rounded-2xl shadow-md p-4 md:p-6 w-full flex flex-row justify-between items-center">
-        <h3 className="text-2xl font-medium">Transfer Stock</h3>
-        <button className="bg-[#615cf6] hover:bg-transparent text-white hover:text-[#615cf6] border border-[#615cf6] px-2 py-1 rounded-md">
-          New Transfer
-        </button>
-      </div>
-      <div className="my-6 w-full flex flex-col md:flex-row justify-between gap-4">
-        <KpiCard
-          title="In Transit"
-          icon={<FaTruck className="mr-2 text-xl text-blue-500" />}
-          value="+ 10,000"
-        />
-        <KpiCard
-          title="Completed"
-          icon={<FaCheckSquare className="mr-2 text-xl text-green-500" />}
-          value="$ 10,000"
-        />
-        <KpiCard
-          title="Pending Approval"
-          icon={<FaHourglassHalf className="mr-2 text-xl text-gray-500" />}
-          value="- 10,000"
-        />
-        <KpiCard
-          title="Cancelled"
-          icon={<FaBan className="mr-2 text-xl text-red-500" />}
-          value="+ 10,000"
-        />
-      </div>
-      <div className="bg-white rounded-2xl shadow-md p-6">
-        <input
-          type="text"
-          placeholder="Search transfers..."
-          className="border border-gray-300 rounded-md px-2 py-1 w-full md:w-2/5"
-        />
-        <TransferTable data={ordersData} />
-        <TransferList data={ordersData} />
-      </div>
-    </main>
-  );
-}
+import { useRouter } from "next/navigation";
+import React from "react";
+import StockTransferForm from "./components/StockTransferForm";
+
+type Props = {};
+
+const AddStockTransfer = (props: Props) => {
+    const { back } = useRouter();
+    return (
+        <main className="p-4 md:p-8">
+            <div className="w-full bg-white rounded-2xl shadow-md p-6 flex flex-col md:flex-row justify-between items-center">
+                <h3 className="text-2xl font-medium mb-4 md:mb-0">
+                    Stock Transfer
+                </h3>
+                <div className="flex flex-row gap-4">
+                    <button
+                        onClick={back}
+                        className="bg-transparent hover:bg-gray-500 text-gray-500 hover:text-white border border-gray-500 px-2 py-1 rounded-md cursor-pointer mr-2"
+                    >
+                        Go Back
+                    </button>
+                </div>
+            </div>
+            <StockTransferForm />
+        </main>
+    );
+};
+
+export default AddStockTransfer;
