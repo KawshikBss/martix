f17fe1f933bd4fb9d1a182572f1760f24691db0e
KawshikBss <kawshikbss@gmail.com>
2026-03-28 00:24:53 +0600

    fixed

--- app/dashboard/components/layout/Navbar/Navbar.tsx
+++ app/dashboard/components/layout/Navbar/Navbar.tsx
@@ -14,27 +14,12 @@ export interface INavbarProps {
 export default function Navbar(props: INavbarProps) {
     const { authUser } = useAuth();
 
-    const markAsRead = (id: number) => {
-        // Handle marking notification as read
-        console.log(`Mark notification ${id} as read`);
-    };
-
     return (
-        <div className="w-full px-8 pt-6 flex flex-row justify-between items-center">
+        <div className="w-full px-8 pt-6 flex flex-row justify-end items-center">
             <IoMdMenu
                 className="w-6 h-6 pointer text-gray-600 hover:text-gray-800 transition-colors duration-200 md:hidden"
                 onClick={props.openSidebar}
             />
-            <div className="hidden md:block">
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="bg-white border border-gray-300 rounded-md px-4 py-1"
-                />
-                <button className="bg-blue-500 text-white rounded-md px-4 py-1 ms-2">
-                    Search
-                </button>
-            </div>
             <div className="flex flex-row items-center gap-8">
                 <Notifications />
 
